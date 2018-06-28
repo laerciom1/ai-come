7f5645162b059e6cd3c66c295f863c3b22aba0b1
Roberto Dantas <7robertodantas@gmail.com>
2018-06-28 13:50:26 -0300

    Endpoint to get address by id

--- aicome-api/src/main/java/br/ufrn/aicome/controller/MeRestController.java
+++ aicome-api/src/main/java/br/ufrn/aicome/controller/MeRestController.java
@@ -65,12 +65,12 @@ public class MeRestController {
 	}
 
 	/**
-	 * Returns the user addresses.
+	 * Returns the user address.
 	 * @return the address with the given id.
 	 */
 	@GetMapping("/addresses/{addressId:[0-9]+}")
-	@ApiOperation(value = "Fetch the addresses by id", response = AddressDTO.class, authorizations=@Authorization("oauth2"))
-	public AddressDTO getUserAddresses(@PathVariable("addressId") Integer addressId, Principal principal){
+	@ApiOperation(value = "Fetch the user address by id", response = AddressDTO.class, authorizations=@Authorization("oauth2"))
+	public AddressDTO getUserAddressById(@PathVariable("addressId") Integer addressId, Principal principal){
 		User user = userRepository.findByUsername(principal.getName()).orElse(new User());
 		Address address = addressRepository.findById(addressId).orElse(new Address());
 		return new AddressDTO(address);
